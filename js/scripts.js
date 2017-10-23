d7bd8d7063bffd95bf37611c0326bc1e60481dee
Amy Churchwell and Colin Worf <amy@>
2017-10-23 14:31:52 -0700

    create form add radio card suit options and create JS function for all

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  $('#cards').submit(function(event){
+    var values = ["ace", 2, 3, 4, 5, 6, 7, 8, 9 , 10, "jack", "queen", "king"];
+    var allsuits = ["hearts", "spades", "diamonds", "clubs"]
+    var suits = $('input[name="suit"]:checked').val();
+    if(suits === "all"){
+      allsuits.forEach(function(suit){
+        values.forEach(function(value){
+          $('.list').append("<li>" + value + " of " + suit + "." + "</li>")
+        });
+      });
+    };
+    event.preventDefault();
+  });
+});
